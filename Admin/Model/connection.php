9303f98e9201861cd5596cec6f41ad66a8943768
buivantuong2k4 <165807916+buivantuong2k4@users.noreply.github.com>
2025-09-15 11:05:09 +0700

    Update connection.php

--- Admin/Model/connection.php
+++ Admin/Model/connection.php
@@ -6,14 +6,16 @@
         function __construct()
         {
             //Thong so ket noi CSDL
-            $severname ="localhost"; 
+              $severname ="hopper.proxy.rlwy.net"; 
             $username ="root";
-            $password =""; 
+            $password ="CecQBHjoyvKAPvjlVAuVpFTfSzooAwTe"; 
             $db_name ="web_mycay";
+			$port="41684";
  
             //Tao ket noi CSDL
-            $this->mysqli = new mysqli($severname,$username,$password,$db_name);
+            $this->mysqli = new mysqli($severname,$username,$password,$db_name,$port);
             $this->mysqli->set_charset("utf8");
+
 
             //check connection
             if ($this->mysqli->connect_error) {
